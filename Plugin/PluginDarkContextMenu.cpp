dafed7000a60c80f8ac75f7f49c1ea20d82eafe1
oZone <positronix10@gmail.com>
2019-11-29 18:23:16 +0100

    Update project structure

--- Plugin/PluginDarkContextMenu.cpp
+++ Plugin/PluginDarkContextMenu.cpp
@@ -7,44 +7,19 @@
 
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
-// WinAPI
-#include <Windows.h>
-
-#include <Uxtheme.h>
-#include <VersionHelpers.h>
-
-// Rainmeter API
-#include "../API/RainmeterAPI.h"
+#include "StdAfx.h"
+#include "PluginDarkContextMenu.h"
 
 // Note: GetString, ExecuteBang and an unnamed function for use as a section variable
 // have been commented out. Uncomment any functions as needed.
 // For more information, see the SDK docs: https://docs.rainmeter.net/developers/plugin/cpp/
-
-const DWORD VER_1809 = 17763; // Windows 10 1809 (October 2018 Update)
-const DWORD VER_1903 = 18362; // Windows 10 1903 (May 2019 Update)
-
-struct Measure {
-    HWND hWnd = nullptr;
-    bool mode = false;
-    bool force = false;
-    bool validVer = false;
-    bool error = false;
-};
-
-enum class PreferredAppMode {
-    Default,
-    AllowDark,
-    ForceDark,
-    ForceLight,
-    Max
-};
 
 inline bool IsAtLeastWin10Build(DWORD buildNumber)
 {
@@ -118,7 +93,7 @@ void SetTheme(struct Measure* measure)
     FreeLibrary(hUxtheme);
 }
 
-inline void SetDefaultMeasureValues(struct Measure* measure)
+void SetDefaultMeasureValues(struct Measure* measure)
 {
     measure->mode = false;
     measure->force = false;
---
+++ Plugin/PluginDarkContextMenu.h
@@ -0,0 +1,40 @@
+/*
+  Copyright (C) 2019 oZone
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+const DWORD VER_1809 = 17763; // Windows 10 1809 (October 2018 Update)
+const DWORD VER_1903 = 18362; // Windows 10 1903 (May 2019 Update)
+
+struct Measure {
+    HWND hWnd = nullptr;
+    bool mode = false;
+    bool force = false;
+    bool validVer = false;
+    bool error = false;
+};
+
+enum class PreferredAppMode {
+    Default,
+    AllowDark,
+    ForceDark,
+    ForceLight,
+    Max
+};
+
+void SetDarkMode(struct Measure* skin, HMODULE hUxtheme);
+void SetTheme(struct Measure* measure);
+inline void SetDefaultMeasureValues(struct Measure* measure);
